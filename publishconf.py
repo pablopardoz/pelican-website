76aecb33bf6f4fe41516bdb9fb7ae9ac85caf2f4
Pablo Pardo <pardozaragoza@gmail.com>
2016-12-16 00:26:52 +0100

    añadido grafo skills

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'vps347190.ovh.net'
+SITEURL = 'http://pablopardoes.s3-website-us-west-2.amazonaws.com/'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
